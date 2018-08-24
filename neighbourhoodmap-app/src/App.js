35b7122243f9ffadea9c36e5144c58d8b4060445
Kyrstle Breslin <kyrstle@live.com.au>
2018-08-24 21:56:35 +0100

    add locations

--- neighbourhoodmap-app/src/App.js
+++ neighbourhoodmap-app/src/App.js
@@ -9,6 +9,47 @@ let latitude = 51.509865;
 let longitude = -0.118092;
 
 export class App extends Component {
+  state = {
+    locations: [
+            {
+                title: 'Hyde Park',
+                park_id: 'xxx',
+                lat: 51.5072715,,
+                lng: -0.167919,
+                visible: true
+            },{
+                title: 'Regents Park',
+                park_id: 'xxx',
+                lat: 51.5302941,
+                lng: -0.1520961,
+                visible: true
+            },{
+                title: 'Richmond Park',
+                park_id: 'xxx',
+                lat: 51.4463902,
+                lng: -0.2779614,
+                visible: true
+            },{
+                title: 'Greenwich Park',
+                park_id: 'xxx',
+                lat: 51.4769128,
+                lng: -0.0007244,
+                visible: true
+            },{
+                title: 'Hampstead Heath',
+                park_id: 'xxx',
+                lat: 51.5608454,
+                lng: -0.1653263,
+                visible: true
+            },{
+                title: 'Battersea Park',
+                park_id: 'xxx',
+                lat: 51.4791108,
+                lng: -0.1586868,
+                visible: true
+            }]
+          }
+  }
 
   static defaultProps = {
       center: {
