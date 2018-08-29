6afe8368d5154c15315fcd6c2b6e87586472be7c
Kyrstle Breslin <kyrstle@live.com.au>
2018-08-29 16:35:24 +0100

    onclick marker showing detailed information.

--- neighbourhoodmap-app/src/App.js
+++ neighbourhoodmap-app/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import GoogleMap from 'google-map-react';
 import SidePanel from './SidePanel.js'
 import Marker from './Marker.js'
+import $ from 'jquery';
 import './App.css';
 
 //https://www.npmjs.com/package/google-map-react
@@ -24,77 +25,66 @@ export class App extends Component {
                   lat : 51.5072682,
                   lng : -0.1657303,
                   cardinal: 'north west',
-                  visible: true
               },{
                   title: 'Regent\'s Park',
                   park_id: '2',
                   lat: 51.5312705,
                   lng: -0.1569694,
                   cardinal: 'north west',
-                  visible: true
               },{
                   title: 'Richmond Park',
                   park_id: '3',
                   lat: 51.4463869,
                   lng: -0.2757727,
                   cardinal: 'south west',
-                  visible: true
               },{
                   title: 'Greenwich Park',
                   park_id: '4',
                   lat: 51.47691,
                   lng: 0.001464,
                   cardinal: 'south east',
-                  visible: true
               },{
                   title: 'Hampstead Heath',
                   park_id: '5',
                   lat: 51.5608421,
                   lng: -0.1631376,
                   cardinal: 'north west',
-                  visible: true
               },{
                   title: 'Battersea Park',
                   park_id: '6',
                   lat: 51.4791075,
                   lng: -0.1564981,
                   cardinal: 'south west',
-                  visible: true
               },{
                   title: 'Victoria Park, London',
                   park_id: '7',
                   lat: 51.5365614,
                   lng: -0.038972,
                   cardinal: 'north east',
-                  visible: true
               },{
                   title: 'Queen Elizabeth Olympic Park',
                   park_id: '8',
                   lat: 51.5432961,
                   lng: -0.0165517,
                   cardinal: 'north east',
-                  visible: true
               },{
                   title: 'Crystal Palace Park',
                   park_id: '9',
                   lat: 51.4218504,
                   lng: -0.0723853,
                   cardinal: 'south east',
-                  visible: true
               },{
                   title: 'Clapham Common',
                   park_id: '10',
                   lat: 51.4587158,
                   lng: -0.1495897,
                   cardinal: 'south west',
-                  visible: true
               },{
                   title: 'Peckham Rye Park',
                   park_id: '11',
                   lat: 51.4595857,
                   lng: -0.0638687,
                   cardinal: 'south east',
-                  visible: true
               }],
         selectedArea: ''
     };
@@ -104,8 +94,10 @@ export class App extends Component {
     console.log('this is selectedArea',selectedArea)
   }
 
-  onMarkerClick = (event) => {
-    console.log(this.title)
+
+  onMarkerClick = (marker, markerid) => {
+    $('.'+markerid).toggle()
+    $('.markerDetails '+markerid).toggle()
   }
 
   render() {
@@ -127,7 +119,7 @@ export class App extends Component {
              return location
            }
          }).map((location) => {
-             return <Marker key={location.park_id} lat={location.lat} lng={location.lng} onClick={() => {console.log('this marker was clicked')}} />
+             return <Marker key={location.park_id} markerid={location.park_id} location={location.title} lat={location.lat} lng={location.lng} onClick={this.onMarkerClick} />
            })
          }
        </GoogleMap>
--- neighbourhoodmap-app/src/Marker.js
+++ neighbourhoodmap-app/src/Marker.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
 
-
 export class Marker extends Component {
 
   render(){
-    return <div className="marker"></div>
+
+    return(
+      <div>
+      <div className="marker" markerid={this.props.markerid} onClick={()=> this.props.onClick(this.props.location, this.props.markerid)} >
+      </div>
+      <div className={"markerDetails "+this.props.markerid}>{this.props.location}</div>
+      </div>
+    )
   }
 }
 
